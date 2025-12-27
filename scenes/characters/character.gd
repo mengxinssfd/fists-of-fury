1f981dcdab57081e13d40039083d329aba163b3c
dyh_a <xinzon32177@163.com>
2025-12-27 16:52:28 +0800

    feat: 实现角色移动及其动画播放

--- scenes/characters/character.gd
+++ scenes/characters/character.gd
@@ -4,7 +4,18 @@ extends CharacterBody2D
 @export var damage: int
 @export var speed: float
 
-func _process(delta: float) -> void:
+#var animation_player
+#func _ready() -> void:
+	#animation_player = get_node("AnimationPlayer")
+	
+@onready var animation_player := $AnimationPlayer 
+@onready var character_sprite := $CharacterSprite
+
+enum State {IDLE, WALK}
+
+var state = State.IDLE
+
+func _process(_delta: float) -> void:
 	#var s = delta * speed
 	#if Input.is_action_pressed("ui_right"):
 		#position += Vector2.RIGHT * s
@@ -14,8 +25,32 @@ func _process(delta: float) -> void:
 		#position += Vector2.UP * s
 	#if Input.is_action_pressed("ui_down"):
 		#position += Vector2.DOWN * s
+	
+	handle_input()
+	handle_movement()
+	handle_animations()	
+	flip_sprites()
+	move_and_slide()
+	
+func handle_movement() -> void:
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
 		
+func handle_input() -> void:
 	var direction := Input.get_vector('ui_left', 'ui_right','ui_up','ui_down')
 	#position += direction * delta * speed
 	velocity = direction* speed
-	move_and_slide()
+	
+func handle_animations() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	elif state == State.WALK:
+		animation_player.play("walk")
+
+func flip_sprites() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	elif velocity.x < 0: 
+		character_sprite.flip_h = true
