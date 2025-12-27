e9ae2306b4396de150bb843992e29fb04ba4713b
dyh_a <xinzon32177@163.com>
2025-12-28 05:52:00 +0800

    feat: 优化摄像机跟随角色移动, 解耦

--- scenes/stage/camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera2D
-
-@export var player :CharacterBody2D
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if player.position.x > position.x:
-		position.x = player.position.x
